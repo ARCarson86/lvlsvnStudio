9762b5a1569263a264506f0ab45a2d2bdffa15ed
Adam Carson <adam.carson@lvlsvn.com>
2014-07-14 15:24:29 -0400

    Finished table creations

---
+++ db/migrate/20140714192246_create_testimonials.rb
@@ -0,0 +1,8 @@
+class CreateTestimonials < ActiveRecord::Migration
+  def change
+    create_table :testimonials do |t|
+    	t.string :client_name
+    	t.text :testimonial
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714180001) do
+ActiveRecord::Schema.define(version: 20140714192246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 20140714180001) do
     t.text     "work_description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
+    t.string "name"
+  end
+
+  create_table "testimonials", force: true do |t|
+    t.string "client_name"
+    t.text   "testimonial"
   end
 
   create_table "users_tables", force: true do |t|
