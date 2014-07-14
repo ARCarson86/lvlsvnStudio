c3361de849ae0eb9c561ee20225a9f5c45b548b1
Adam Carson <adam.carson@lvlsvn.com>
2014-07-14 15:26:14 -0400

    Added rows to skills, testimonials tables

--- db/migrate/20140714192150_create_skills.rb
+++ db/migrate/20140714192150_create_skills.rb
@@ -2,6 +2,7 @@ class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
     	t.string :name
+    	t.timestamps
     end
   end
 end
--- db/migrate/20140714192246_create_testimonials.rb
+++ db/migrate/20140714192246_create_testimonials.rb
@@ -3,6 +3,7 @@ class CreateTestimonials < ActiveRecord::Migration
     create_table :testimonials do |t|
     	t.string :client_name
     	t.text :testimonial
+    	t.timestamps
     end
   end
 end
