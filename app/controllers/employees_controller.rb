39b84e5963b0c7acd0813b63c688aebef97036b0
Mike Burkle <Michael.burkle@lvlsvn.com>
2014-07-23 10:41:42 -0400

    added comments

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -62,12 +62,10 @@ class EmployeesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_employee
       @employee = Employee.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
       params.require(:employee).permit(:id, :first_name, :middle_initial, :last_name, :job_title, :bio, :email, :hired_at, :created_at, :update_at)
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
 	belongs_to :users
+	has_many :comments, :as => :commentable
 end
