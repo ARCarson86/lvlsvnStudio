8966fbc680097b994181f3a97778682a38e17144
Sam Castelaz <sam.castelaz@lvlsvn.com>
2014-07-23 08:56:09 -0400

    Added Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_authorization_check :except => [:new, :create]
+  skip_authorization_check :except => [:new, :destory]
 
   # GET /users
   # GET /users.json
